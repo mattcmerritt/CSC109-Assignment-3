6cf70d7bf0ea3308f5b8fb9f886792e36ffde119
mikepmerritt <mikepmerritt@gmail.com>
2021-02-12 00:56:02 -0500

    added Snack class and a variety of Snack objects

--- src/Test.java
+++ src/Test.java
@@ -1,4 +1,11 @@
 
 public class Test {
-
+	public static void main (String[] args) {
+		Snack chips = new Snack("Potato Chips");
+		Snack doritos = new Snack("Doritos");
+		Snack oreos = new Snack("Oreos");
+		Snack water = new Snack("Water");
+		Snack pepsi = new Snack("Pepsi");
+		Snack gatorade = new Snack("Gatorade");
+	}
 }
